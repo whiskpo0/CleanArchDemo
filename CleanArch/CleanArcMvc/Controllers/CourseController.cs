21643a3ab9550d9269cb7d4df3d4614b69031f45
Adam M <whiskypo082@gmail.com>
2020-05-01 09:40:05 -0500

    Adding Images and CSS styling, Added CourseController.

---
+++ CleanArch/CleanArcMvc/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Services;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArcMvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService; 
+        
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService; 
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCourses(); 
+            
+            return View(model);
+        }
+    }
+}
